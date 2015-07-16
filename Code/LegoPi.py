8f269b8059ac32e969a33068142ed39c45153316
Joshua Vecsei <j.vecsei@gmx.de>
2015-07-16 23:18:41 +0200

    alles über den adwandler abwickeln

--- Code/LegoPi.py
+++ Code/LegoPi.py
@@ -8,11 +8,9 @@ import time
 import os
 
 # ports for motors and sensors
-class Port(Enum):
+class MotorPort(Enum):
     A = [16, 15]
     B = [23, 26]
-    C = 3
-    D = 4
 
 
 # global functions 
@@ -24,21 +22,17 @@ def delay(ms):
 # sensors
 class Sensor:
     
-    _port = None
+    _channel = None
     
-    def __init__(self, port):
-        assert isinstance(port, Port)
-        self._setupPins(port)
-        self._port = port;
-
-class LightSensor(Sensor):
-    
-    _spi = None
-    
-    def __init__(self, port):
-        super().__init__(port)
+    def __init__(self, channel):
+        self._validateChannel(channel)        
+        self._channel = channel;
         self._openSPIBus()
-    
+        
+    def _validateChannel(self, channel):
+        if(channel > 7 || channel < = 0)
+            raise ValueError("channel must be a value between 0 and 7")
+            
     def _openSPIBus(self):
         self._spi = spidev.SpiDev()
         spi.open(0,0)
@@ -53,63 +47,60 @@ class LightSensor(Sensor):
         volts = round(volts,places)
         return volts
     
-    def getValue(self, lightChannel):
-        lightLevel = 1024 - _readChannel(lightChannel)
-        lightVolts = _convertVolts(lightLevel,2)
-        return lightVolts
+    def getValue(self):
+        level = 1024 - _readChannel(_channel)
+        volts = _convertVolts(level,2)
+        return volts
 
+
+class LightSensor(Sensor):
+    
+    _spi = None
+    
+    def getValue(self):
+        return super(LightSensor, self).getValue()
+    
 class TouchSensor(Sensor):
     
-    def __init__(self, port):
-        super().__init__(port)
-        self._setupPin()
-    
-    def _setupPin(self):
-        GPIO.setmode(GPIO.BCM) 
-        GPIO.setup(_self.port.value[0], GPIO.IN, pull_up_down = GPIO.PUD_UP)
-    
-    def isPressed():
-        if GPIO.input(_self.port.value[0]):
-            return false
-        else:
-            return true
+    def isPressed(self):
+        return (super(LightSensor, self).getValue() == 1)
     
 # motor
 class Motor:
     
-    _port = None
+    _motorPort = None
     _speed = 50
     
-    def __init__(self, port):
-        assert isinstance(port, Port)
-        self._port = port;
+    def __init__(self, motorPort):
+        assert isinstance(motorPort, MotorPort)
+        self._motorPort = motorPort;
         self._setupPins()
     
     def _setupPins(self):
         GPIO.setmode(GPIO.BCM)       
-        GPIO.setup(self._port.value[0], GPIO.OUT)    
-        GPIO.setup(self._port.value[1], GPIO.OUT)
-        pin1 = GPIO.PWM(self._port.value[0], self._speed) // frequency 50HZ by default for both pins
-        pin2 = GPIO.PWM(self._port.value[1], self._speed)
+        GPIO.setup(self._motorPort.value[0], GPIO.OUT)    
+        GPIO.setup(self._motorPort.value[1], GPIO.OUT)
+        pin1 = GPIO.PWM(self._motorPort.value[0], self._speed) // frequency 50HZ by default for both pins
+        pin2 = GPIO.PWM(self._motorPort.value[1], self._speed)
         pin1.ChangeDutyCycle(self._speed)
         pin2.ChangeDutyCycle(self._speed)
     
     def forward(self):
-        GPIO.output(self._port.value[1], GPIO.LOW)
+        GPIO.output(self._motorPort.value[1], GPIO.LOW)
         time.sleep(0.01)
-        GPIO.output(self._port.value[0], GPIO.HIGH)
+        GPIO.output(self._motorPort.value[0], GPIO.HIGH)
         time.sleep(0.01)
         
     def backwards(self):
-        GPIO.output(self._port.value[0], GPIO.LOW)
+        GPIO.output(self._motorPort.value[0], GPIO.LOW)
         time.sleep(0.01)
-        GPIO.output(self._port.value[1], GPIO.HIGH)
+        GPIO.output(self._motorPort.value[1], GPIO.HIGH)
         time.sleep(0.01)
         
     def stop(self):
-        GPIO.output(self._port.value[0], GPIO.LOW)
+        GPIO.output(self._motorPort.value[0], GPIO.LOW)
         time.sleep(0.01)
-        GPIO.output(self._port.value[1], GPIO.LOW)
+        GPIO.output(self._motorPort.value[1], GPIO.LOW)
         time.sleep(0.01)
         
     def setSpeed(self, speed):
