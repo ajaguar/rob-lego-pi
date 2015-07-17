a7c3071ccd375f664f4dd9fa3995c82d38170121
Joshua Vecsei <j.vecsei@gmx.de>
2015-07-17 14:49:30 +0200

    fixes

--- Code/LegoPi.py
+++ Code/LegoPi.py
@@ -2,15 +2,14 @@
 
 import time
 import RPi.GPIO as GPIO
-from enum import Enum
 import spidev
 import time
 import os
 
 # ports for motors and sensors
-class MotorPort(Enum):
+class MotorPort:
     A = [16, 15]
-    B = [23, 26]
+    B = [22, 18]
 
 
 # global functions 
@@ -31,38 +30,38 @@ class Sensor:
         self._openSPIBus()
         
     def _validateChannel(self, channel):
-        if(channel > 7 || channel < = 0)
+        if(channel > 7 or channel < 0):
             raise ValueError("channel must be a value between 0 and 7")
             
     def _openSPIBus(self):
         self._spi = spidev.SpiDev()
-        spi.open(0,0)
+        self._spi.open(0,0)
     
     def _readChannel(self, channel):
         adc = self._spi.xfer2([1,(8+channel)<<4,0])
         data = ((adc[1]&3) << 8) + adc[2]
         return data
     
-    def _convertVolts(data,places):
+    def _convertVolts(self, data,places):
         volts = (data * 3.3) / float(1023)
         volts = round(volts,places)
         return volts
     
     def getValue(self):
-        level = 1024 - _readChannel(_channel)
-        volts = _convertVolts(level,2)
+        level = 1024 - self._readChannel(self._channel)
+        volts = self._convertVolts(level,2)
         return volts
 
 
 class LightSensor(Sensor):
     
     def getValue(self):
-        return super(LightSensor, self).getValue()
+        return Sensor.getValue(self)
     
 class TouchSensor(Sensor):
     
     def isPressed(self):
-        return (super(LightSensor, self).getValue() == 1)
+        return (Sensor.getValue(self) > 1)
     
 # motor
 class Motor:
@@ -71,35 +70,34 @@ class Motor:
     _speed = 50
     
     def __init__(self, motorPort):
-        assert isinstance(motorPort, MotorPort)
         self._motorPort = motorPort;
         self._setupPins()
     
     def _setupPins(self):
-        GPIO.setmode(GPIO.BCM)       
-        GPIO.setup(self._motorPort.value[0], GPIO.OUT)    
-        GPIO.setup(self._motorPort.value[1], GPIO.OUT)
-        pin1 = GPIO.PWM(self._motorPort.value[0], self._speed) // frequency 50HZ by default for both pins
-        pin2 = GPIO.PWM(self._motorPort.value[1], self._speed)
+        GPIO.setmode(GPIO.BOARD)       
+        GPIO.setup(self._motorPort[0], GPIO.OUT)    
+        GPIO.setup(self._motorPort[1], GPIO.OUT)
+        pin1 = GPIO.PWM(self._motorPort[0], self._speed) # frequency 50HZ by default for both pins
+        pin2 = GPIO.PWM(self._motorPort[1], self._speed)
         pin1.ChangeDutyCycle(self._speed)
         pin2.ChangeDutyCycle(self._speed)
     
     def forward(self):
-        GPIO.output(self._motorPort.value[1], GPIO.LOW)
+        GPIO.output(self._motorPort[1], GPIO.LOW)
         time.sleep(0.01)
-        GPIO.output(self._motorPort.value[0], GPIO.HIGH)
+        GPIO.output(self._motorPort[0], GPIO.HIGH)
         time.sleep(0.01)
         
     def backward(self):
-        GPIO.output(self._motorPort.value[0], GPIO.LOW)
+        GPIO.output(self._motorPort[0], GPIO.LOW)
         time.sleep(0.01)
-        GPIO.output(self._motorPort.value[1], GPIO.HIGH)
+        GPIO.output(self._motorPort[1], GPIO.HIGH)
         time.sleep(0.01)
         
     def stop(self):
-        GPIO.output(self._motorPort.value[0], GPIO.LOW)
+        GPIO.output(self._motorPort[0], GPIO.LOW)
         time.sleep(0.01)
-        GPIO.output(self._motorPort.value[1], GPIO.LOW)
+        GPIO.output(self._motorPort[1], GPIO.LOW)
         time.sleep(0.01)
         
     def setSpeed(self, speed):
--- Code/Run.py
+++ Code/Run.py
@@ -1,32 +1,48 @@
 from LegoPi import *
 
-lightSensor = new LightSensor(0)
-touchSensor = new TouchSensor(1)
-motorA = new Motor(MotorPort.A)
-motorB = new Motor(MotorPort.B)
+#lightSensor = LightSensor(0)
+#i = 0;
+#while i < 10:
+#    print(lightSensor.getValue())
+#    i += 1
+#    delay(500)
 
-while !touchSensor.isPressed():
-    print(lightSensor.getValue())
-    delay(1000)
-
-print("TouchSensor is pressed")
-
+motorA = Motor(MotorPort.A)
 motorA.forward()
-delay(1000)
-
-motorA.backward()
-delay(1000)
-
+motorB = Motor(MotorPort.B)
 motorB.forward()
-delay(1000)
 
-motorB.backward()
 delay(1000)
+motorA.stop()
+motorB.stop()
 
-motorA.forward()
-motorB.forward()
-delay(1000)
+#lightSensor = new LightSensor(0)
+#touchSensor = new TouchSensor(1)
+#motorA = new Motor(MotorPort.A)
+#motorB = new Motor(MotorPort.B)
 
-motorA.backward()
-motorB.backward()
-delay(1000)
+#while !touchSensor.isPressed():
+#    print(lightSensor.getValue())
+#    delay(1000)
+
+#print("TouchSensor is pressed")
+
+#motorA.forward()
+#delay(1000)
+
+#motorA.backward()
+#delay(1000)
+
+#motorB.forward()
+#delay(1000)
+
+#motorB.backward()
+#delay(1000)
+
+#motorA.forward()
+#motorB.forward()
+#delay(1000)
+
+#motorA.backward()
+#motorB.backward()
+#delay(1000)
